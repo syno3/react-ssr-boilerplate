57ba09575a308850082862a39dbced3be3af5567
syno3 <sinotechxvista@gmail.com>
2022-07-11 22:28:21 +0300

    setup dependancies

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export default function App() {
+  return <h2>React from scratch working</h2>;
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
